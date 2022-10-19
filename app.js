e965bf4349a79db40fb70fe7719071361cc2f585
Kopen <89531508+kopen1@users.noreply.github.com>
2022-10-19 22:56:30 +0700

    update edition

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import komik from "./Api/komik.js"
 import express from "express"
 import fs from "fs"
 const ap =  express();
+const port = process.env.PORT || 8080;
+
+
 
 ap.use(express.static("./public"))
 ap.use("/komik",komik)
@@ -10,6 +13,6 @@ res.writeHead(200,{"Content-Type":"text/html"});
 fs.createReadStream("./public/index.html","UTF-8").pipe(res);
 })
 
-ap.listen(8080, ()=>{
+ap.listen(port, ()=>{
   console.log(" Starting....")
 })
